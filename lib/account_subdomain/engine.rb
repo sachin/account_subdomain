43ed9fcf8e78d7cea209b583ed31bc9de9119d28
sachin choudhary <sachin238@gmail.com>
2014-09-23 11:31:18 +0530

    include helper through engine

--- lib/account_subdomain/engine.rb
+++ lib/account_subdomain/engine.rb
@@ -4,6 +4,7 @@ module AccountSubdomain
     initializer  "account_subdomain.load_helpers" do
       ActiveSupport.on_load(:action_controller) do
         include AccountSubdomain::AccountsHelper
+        helper AccountSubdomain::Engine.helpers
       end
     end
   end
