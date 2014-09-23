f7fe9dac1ddd06605ca0a93ff0bc39227d3a3f7b
sachin choudhary <sachin238@gmail.com>
2014-09-23 11:32:34 +0530

    deleting files

--- test/dummy/db/migrate/20140912100451_create_account_subdomain_accounts.rb
+++
@@ -1,9 +0,0 @@
-class CreateAccountSubdomainAccounts < ActiveRecord::Migration
-  def change
-    create_table :account_subdomain_accounts do |t|
-      t.string :subdomain
-
-      t.timestamps
-    end
-  end
-end
