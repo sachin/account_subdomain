12b48361116d5e7592e3baf9997429ca4f6fa7d3
sachin choudhary <sachin238@gmail.com>
2014-10-28 23:02:57 +0530

    changes done for index pages

--- test/dummy/config/initializers/account_subdomain.rb
+++ test/dummy/config/initializers/account_subdomain.rb
@@ -4,11 +4,11 @@
 #You can give an array to each type of relationships.
 #
 #AccountSubdomain supports every type of relationships which rails supports
-AccountSubdomain.reflections << "has_many :users, class_name: 'users', dependent: :destroy"
-AccountSubdomain.reflections << "before_save :testsachin"
+AccountSubdomain.reflections << "has_many :users, class_name: 'User', foreign_key: 'account_id',dependent: :destroy"
+
 
 #This setting is used for redirect for invalid subdomain.
-AccountSubdomain.redirect_path = '/'
+AccountSubdomain.redirect_path = 'main_app.root_path'
 
 #This setting is used for redirect after successfully creation of subdomain.
 AccountSubdomain.after_creation_url = 'account_subdomain.accounts_url'
@@ -24,5 +24,6 @@ AccountSubdomain.tld_length = 1
 #Settings for filters
 #use case :- You want the https authentication before the access the account subdomain
 #AccountSubdomain.action_controller_filters[:before_filter] = [:validate_http_access_only]
+#
 # Add validate_http_access_only filter in your application.rb
 AccountSubdomain.action_controller_filters[:before_filter] = []
